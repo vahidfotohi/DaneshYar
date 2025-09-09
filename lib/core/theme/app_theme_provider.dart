d565ad64a8895b2ef621aeddcd9d87b3da7bb352
vahid fotohi <vahidfotohi0@gmail.com>
2025-09-09 13:51:28 +0330

    fix(theme): fix bug app_theme_provider

--- lib/core/theme/app_theme_provider.dart
+++ lib/core/theme/app_theme_provider.dart
@@ -2,42 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-// 1. Provider
-final themeNotifierProvider = NotifierProvider<ThemeNotifier, ThemeMode>(() {
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, ThemeMode>((
+  ref,
+) {
   return ThemeNotifier();
 });
 
-// 2. Notifier
-class ThemeNotifier extends Notifier<ThemeMode> {
-  static const _themePrefsKey = 'themeMode';
+class ThemeNotifier extends StateNotifier<ThemeMode> {
+  static const _themePrefsKey = 'theme_mode';
 
-  @override
-  ThemeMode build() {
-    // Load the theme mode from storage when the provider is first created
-    _loadThemeFromPrefs();
-    return ThemeMode.light; // Default value
+  ThemeNotifier() : super(ThemeMode.light);
+
+  Future<void> _saveTheme(ThemeMode themeMode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(
+      _themePrefsKey,
+      themeMode == ThemeMode.dark ? 'dark' : 'light',
+    );
   }
 
-  Future<void> _loadThemeFromPrefs() async {
+  Future<void> loadTheme() async {
     final prefs = await SharedPreferences.getInstance();
-    final themeIndex = prefs.getInt(_themePrefsKey);
-    if (themeIndex != null) {
-      state = ThemeMode.values[themeIndex];
+    final themeString = prefs.getString(_themePrefsKey);
+
+    if (themeString == 'dark') {
+      state = ThemeMode.dark;
+    } else {
+      state = ThemeMode.light;
     }
   }
 
-  Future<void> _saveThemeToPrefs(ThemeMode themeMode) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt(_themePrefsKey, themeMode.index);
-  }
-
   void toggleTheme() {
-    state = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
-    _saveThemeToPrefs(state);
-  }
-
-  void setTheme(ThemeMode themeMode) {
-    state = themeMode;
-    _saveThemeToPrefs(state);
+    state = (state == ThemeMode.light) ? ThemeMode.dark : ThemeMode.light;
+    _saveTheme(state);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:daneshyar/core/theme/app_theme.dart';
 import 'package:daneshyar/core/theme/app_theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/routes/app_route.dart';
 import 'features/authentication/user/provider/user_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   final container = ProviderContainer();
   await container.read(userProvider.notifier).loadUserFromPrefs();
-   container.read(themeNotifierProvider.notifier);
+  await container.read(themeNotifierProvider.notifier).loadTheme();
 
   runApp(UncontrolledProviderScope(container: container, child: const MyApp()));
 }
@@ -24,12 +26,9 @@ class MyApp extends ConsumerWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "daneshYar",
-
-      // 4. Set theme properties
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: themeMode,
-
       onGenerateRoute: AppRoute.generateRoute,
       initialRoute: AppRoute.initialDeciderScreen,
     );
